ec3a3acfb9b32dba68d0d5126483c2440a64c9c4
Vz-3 <vondc6201@gmail.com>
2024-04-23 12:32:11 +0800

    Add auth controller

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+export async function register() {
+
+};
+
+export async function deleteAccount() {
+
+};
+
+export async function logIn() {
+
+};
+
+export async function logOut() {
+
+};
+
+export async function update() {
+
+};
