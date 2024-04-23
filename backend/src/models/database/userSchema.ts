b3dac00a810546590f8336b5b7d2851908d78686
Vz-3 <vondc6201@gmail.com>
2024-04-23 21:52:56 +0800

    Add userSchema

---
+++ backend/src/models/database/userSchema.ts
@@ -0,0 +1,42 @@
+import { Schema, Types } from "mongoose";
+
+export enum Role {
+    ADMIN = 'admin',
+    OWNER = 'owner',
+    TENANT = 'tenant'
+}
+
+// Typescript types
+export interface User {
+    _id?: Types.ObjectId,
+    userName: string,
+    password: string,
+    name: {
+        firstName: string,
+        middleName?: string,
+        lastName: string,
+    },
+    email: string,
+    contactNo: string,
+    dateOfBirth: Date,
+    role: Role,
+    unitNo?: Number,
+    properties?: Types.ObjectId[],
+}
+
+// Mongoose Schema
+export const userSchema: Schema<User> = new Schema({
+        userName: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        name: {
+            firstName: { type: String, required: true },
+            middleName: { type: String },
+            lastName: { type: String, required: true }
+        },
+        email: { type: String, required: true, unique: true },
+        contactNo: { type: String, required: true },
+        dateOfBirth: { type: Date, required: true },
+        role: { type: String, required: true, enum: Object.values(Role), default: Role.TENANT},
+        unitNo: { type: Number },
+        properties: { type: [{ type: Types.ObjectId, ref: 'Property' }], default: []}
+});
