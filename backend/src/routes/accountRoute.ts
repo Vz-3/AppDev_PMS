64de49122d6f439ea444b031e85ab5a3a95abd47
Vz-3 <vondc6201@gmail.com>
2024-04-23 17:02:04 +0800

    Rename auth to account

---
+++ backend/src/routes/accountRoute.ts
@@ -0,0 +1,13 @@
+//@ts-nocheck
+import { Router } from "express";
+import { register, deleteAccount, logIn, logOut, update } from "../controllers/accountController";
+
+const router = Router();
+
+router.post("/register", register);
+router.patch("/update", update);
+router.delete("/delete", deleteAccount);
+router.post("/login", logIn);
+router.post("/logout", logOut);
+
+export default router;
