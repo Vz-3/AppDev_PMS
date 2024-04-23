69fc9c93984edeb621c7c7d9a20ccea533dda1bf
Vz-3 <vondc6201@gmail.com>
2024-04-23 12:18:39 +0800

    Add auth routes

---
+++ backend/src/routes/authRoute.ts
@@ -0,0 +1,10 @@
+//@ts-nocheck
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/register", );
+router.patch("/update", );
+router.delete("/delete", );
+router.post("/login", );
+router.post("/logout", );
