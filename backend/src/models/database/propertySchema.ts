04270d0a48cb4f9ed9820fb426630aeb25409db4
Vz-3 <vondc6201@gmail.com>
2024-05-06 17:55:07 +0800

    Add model and schema for property

--- backend/src/models/database/propertySchema.ts
+++ backend/src/models/database/propertySchema.ts
@@ -1,8 +1,10 @@
 import { Schema, Types } from "mongoose";
 
+export type PaymentHistory = { date: Date, amount: Number };
+
 export enum BuildingType {
     APARTMENT = 'apartment',
-    CONDO = 'condo'
+    CONDOMINIUM = 'condominium'
 }
 
 export enum UnitStatus {
@@ -11,51 +13,53 @@ export enum UnitStatus {
     MAINTENANCE = 'maintenance'
 }
 
+export enum UnitType {
+    QUADRA = 'quadra',
+    DUO = 'duo'
+}
 // Typescript types
 export interface Building {
     _id?: Types.ObjectId,
     buildingType: BuildingType,
     buildingName: string,
-    buildingOwner: string, // Technically owner email or ID from user schema
+    buildingOwner: string, // Technically owner ID from user schema
     address1: string,
     address2?: string,
     city: string,
     noOfUnits: Number,
-    tenants: Types.ObjectId[], // List of tenant IDs from user schema
     misc?: string,
-    createdAt?: Date
 }
 
 export interface Unit {
     _id?: Types.ObjectId,
     unitNo: Number,
+    unitType: UnitType,
     status: UnitStatus,
     buildingId: string,
     unitMonthlyCost: Number,
-    tenant: string // List of tenant IDs from user schema
+    tenant: string 
     modeOfPayment: string, // link to qr code image? bare bones payment implementation
     leastStartDate?: Date,
     leastEndDate?: Date,
-    paymentHistory?: [{ date: Date, amount: Number }],
+    paymentHistory?: PaymentHistory[],
     misc?: string,
 }
 
 // Mongoose Schema
 export const buildingSchema: Schema<Building> = new Schema({
     buildingType: { type: String, required: true, enum: Object.values(BuildingType), default: BuildingType.APARTMENT},
-    buildingName: { type: String, required: true},
+    buildingName: { type: String, required: true, unique: true},
     buildingOwner: { type: String, required: true}, 
     address1: { type: String, required: true},
     address2: { type: String },
     city: { type: String, required: true}, // should be a dropdown list
     noOfUnits: { type: Number, required: true},
-    tenants: { type: [{ type: Types.ObjectId, ref: 'Tenants'}], required: true},
     misc: { type: String },
-    createdAt: { type: Date}
 });
 
 export const unitSchema: Schema<Unit> = new Schema({
     unitNo: { type: Number, required: true},
+    unitType: { type: String, required: true, enum: Object.values(UnitType), default: UnitType.QUADRA},
     status: { type: String, required: true, enum: Object.values(UnitStatus), default: UnitStatus.VACANT},
     buildingId: { type: String, ref: 'Building', required: true},
     unitMonthlyCost: { type: Number, required: true},
@@ -63,6 +67,6 @@ export const unitSchema: Schema<Unit> = new Schema({
     modeOfPayment: { type: String, required: true},
     leastStartDate: { type: Date },
     leastEndDate: { type: Date },
-    paymentHistory: { type: [{ date: Date, amount: Number }]},
+    paymentHistory: { type: Array<PaymentHistory>()},
     misc: { type: String },
 });
